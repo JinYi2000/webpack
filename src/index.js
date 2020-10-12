ed5fa77c1a0a8c0fe062c653988f6f9537bc5efc
JinYi2000 <1439261382@qq.com>
2020-10-12 09:49:40 +0800

    loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import _ from 'lodash'
+import a from './style.css'
 
 function component(){
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');//拼接字符串   演示如何使用这个库
+    element.classList.add('hello');
     return element
 }
 
 document.body.appendChild(component());
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
 
-
 module.exports = {
     entry:'./src/index.js',
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    
+                ]
+            }
+        ]
+    },
     output:{
         filename:'bundle.js',
         path:path.resolve(__dirname,'dist')//因为mac、windows下目录不一样   绝对路径
-    }
+    },
+
 };
